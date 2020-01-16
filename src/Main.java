09474daa92e22b15d8d2ffefaec0951274ea5ca9
JelleStoof <jellestoof@gmail.com>
2020-01-16 11:31:41 +0100

    deleted damn it works

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         // TODO DELETE THIS!!!
-        System.out.println("damm it worksxxxxxsssss");
 
         BusinessRuleGenerator x = new OracleBusinessRuleGenerator();
         System.out.println(x.example(2));
