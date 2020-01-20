3bee48d37fe17e06913a721d78df9fb046fc00ce
JelleStoof <jellestoof@gmail.com>
2020-01-20 11:11:59 +0100

    made execute work

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BusinessRuleGenerator x = new OracleBusinessRuleGenerator();
-        System.out.println(x.example(2));
+        System.out.println(x.execute(2));
 //        System.out.println(x.example(3));
     }
 }
