382051410be528f84c20c5cb13ac575c344c424a
stefanVanEchtelt <stefan.vanechtelt@student.hu.nl>
2020-01-20 10:59:27 +0100

    code cleanup

--- src/rule/RuleFactory.java
+++ src/rule/RuleFactory.java
@@ -23,16 +23,22 @@ public class RuleFactory implements RuleFactoryInterface {
 
         if (this.ruleTypeEid == Or.getRuleTypeEid()) {
             rule = new Or(this.rule);
+
         } else if (this.ruleTypeEid == Not.getRuleTypeEid()) {
             rule = new Not(this.rule);
+
         } else if (this.ruleTypeEid == Equals.getRuleTypeEid()) {
             rule = new Equals(this.rule, values.get(0));
+
         } else if (this.ruleTypeEid == LessThan.getRuleTypeEid()) {
             rule = new LessThan(this.rule, values.get(0));
+
         } else if (this.ruleTypeEid == In.getRuleTypeEid()) {
             rule = new In(this.rule, values);
+
         } else if (this.ruleTypeEid == LargerThan.getRuleTypeEid()) {
             rule = new LargerThan(this.rule, values.get(0));
+
         }
 
         return rule;
