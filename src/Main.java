a12249a430625adf860b24a71d25adf05ee8da1b
stefanVanEchtelt <stefan.vanechtelt@student.hu.nl>
2020-01-23 15:45:51 +0100

    logs

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         OracleBusinessRuleGenerator x = new OracleBusinessRuleGenerator();
 //        System.out.println(x.delete(2));
-        System.out.println(x.execute(2));
-//        System.out.println(x.example(3)s);
+//        System.out.println(x.execute(2));
+        System.out.println(x.example(3));
     }
 }
