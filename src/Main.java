be7433b03f9663059c6f0d48b3a858b2ba003821
stefanVanEchtelt <stefan.vanechtelt@student.hu.nl>
2020-01-28 12:10:12 +0100

    change in with quates

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         OracleBusinessRuleGenerator x = new OracleBusinessRuleGenerator();
 //        System.out.println(x.delete(2));
-        System.out.println(x.example(2));
+        System.out.println(x.example(223));
 //        System.out.println(x.execute(2));
     }
 }
